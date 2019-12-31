9fa1b96012c297f68f309b75a6541f4d4ecbc934
williammian <william_mian@yahoo.com.br>
2019-12-31 16:59:13 -0300

    conexao com banco de dados, tratamento de erros globais, CORS

--- src/app.js
+++ src/app.js
@@ -1,24 +1,33 @@
+const cors = require("cors");
+
 const http = require("http");
 const express = require("express");
+const status = require("http-status");
 const spoilersRoute = require("./routes/spoilers");
+const sequelize = require("./database/database");
 
 const app = express();
 
-const hostname = '127.0.0.1';
-const port = 3000;
-
-app.set("port", port);
+app.use(cors());
 
 app.use(express.json());
 
 app.use("/api", spoilersRoute);
 
 app.use((request, response, next) => {
-    response.status(404).send();
+  response.status(status.NOT_FOUND).send();
 });
 
-const server = http.createServer(app);
+app.use((error, request, response, next) => {
+  response.status(status.INTERNAL_SERVER_ERROR).json({ error });
+});
 
-server.listen(port, hostname, () => {
-    console.log(`Servidor em execução em http://${hostname}:${port}/`);
+sequelize.sync({ force: true }).then(() => {
+  const port = process.env.PORT || 3000;
+
+  app.set("port", port);
+
+  const server = http.createServer(app);
+
+  server.listen(port);
 });
